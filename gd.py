e3d9d675f403b8820855e92ba1aeb7f367f7a6fc
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-14 13:22:03 +0545

    gradient descent functions

--- gd.py
+++ gd.py
@@ -78,53 +78,53 @@ compute_gradient_test(compute_gradient)
 
 # print('Gradient at test w, b:', tmp_dj_dw, tmp_dj_db)
 
-# def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
-#     # number of training examples
-#     m = len(x)
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    # number of training examples
+    m = len(x)
     
-#     # An array to store cost J and w's at each iteration for graphing later
-#     J_history = []
-#     w_history = []
-#     w = copy.deepcopy(w_in)  #avoid modifying global w within function
-#     b = b_in
+    # An array to store cost J and w's at each iteration for graphing later
+    J_history = []
+    w_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
     
-#     for i in range(num_iters):
+    for i in range(num_iters):
 
-#         # Calculate the gradient and update the parameters
-#         dj_dw, dj_db = gradient_function(x, y, w, b )  
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = gradient_function(x, y, w, b )  
 
-#         # Update Parameters using w, b, alpha and gradient
-#         w = w - alpha * dj_dw               
-#         b = b - alpha * dj_db               
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
 
-#         # Save cost J at each iteration
-#         if i<100000:      # prevent resource exhaustion 
-#             cost =  cost_function(x, y, w, b)
-#             J_history.append(cost)
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(x, y, w, b)
+            J_history.append(cost)
 
-#         # Print cost every at intervals 10 times or as many iterations if < 10
-#         if i% math.ceil(num_iters/10) == 0:
-#             w_history.append(w)
-#             print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            w_history.append(w)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
         
-#     return w, b, J_history, w_history #return w and J,w history for graphing
+    return w, b, J_history, w_history #return w and J,w history for graphing
 
-# # initialize fitting parameters. Recall that the shape of w is (n,)
-# initial_w = 0.
-# initial_b = 0.
+# initialize fitting parameters. Recall that the shape of w is (n,)
+initial_w = 0.
+initial_b = 0.
 
-# # some gradient descent settings
-# iterations = 1500
-# alpha = 0.01
+# some gradient descent settings
+iterations = 1500
+alpha = 0.01
 
-# w,b,_,_ = gradient_descent(x_train ,y_train, initial_w, initial_b, 
-#                      compute_cost, compute_gradient, alpha, iterations)
-# print("w,b found by gradient descent:", w, b)
+w,b,_,_ = gradient_descent(x_train ,y_train, initial_w, initial_b, 
+                     compute_cost, compute_gradient, alpha, iterations)
+print("w,b found by gradient descent:", w, b)
 
-# #now plotting the data to find the best fit
+#now plotting the data to find the best fit
 
-# m = x_train.shape[0]
-# predicted = np.zeros(m)
+m = x_train.shape[0]
+predicted = np.zeros(m)
 
 # for i in range(m):
 #     predicted[i] = w * x_train[i] + b
