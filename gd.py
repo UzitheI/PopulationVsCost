6e2d50ce643a975960071ccbf3fcce0df12c3e30
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-14 13:22:29 +0545

    plotting and prediction

--- gd.py
+++ gd.py
@@ -126,28 +126,28 @@ print("w,b found by gradient descent:", w, b)
 m = x_train.shape[0]
 predicted = np.zeros(m)
 
-# for i in range(m):
-#     predicted[i] = w * x_train[i] + b
+for i in range(m):
+    predicted[i] = w * x_train[i] + b
 
-# # Create a scatter plot of the data. 
+# Create a scatter plot of the data. 
     
-# plt.plot(x_train, predicted, c = "b")
-# plt.scatter(x_train, y_train, marker='x', c='r') 
+plt.plot(x_train, predicted, c = "b")
+plt.scatter(x_train, y_train, marker='x', c='r') 
 
-# # Set the title
-# plt.title("Profits vs. Population per city")
-# # Set the y-axis label
-# plt.ylabel('Profit in $10,000')
-# # Set the x-axis label
-# plt.xlabel('Population of City in 10,000s')
+# Set the title
+plt.title("Profits vs. Population per city")
+# Set the y-axis label
+plt.ylabel('Profit in $10,000')
+# Set the x-axis label
+plt.xlabel('Population of City in 10,000s')
 
-# plt.show()
+plt.show()
 
-# print('Here, we can now see a graph that shows the best linear fit for the model')
+print('Here, we can now see a graph that shows the best linear fit for the model')
 
-# print('Now we can make essential predictions with the help of the model')
+print('Now we can make essential predictions with the help of the model')
 
-# print('Give an appropriate value,ie 3.5=35000 or 1=10000')
-# population=float(input())
-# predict = population * w + b
-# print('For population',population,' we predict a profit of $%.2f' % (predict*10000))
+print('Give an appropriate value,ie 3.5=35000 or 1=10000')
+population=float(input())
+predict = population * w + b
+print('For population',population,' we predict a profit of $%.2f' % (predict*10000))
